ac7104cd89635ff6b2fe0bdfbaecf0586012a81e
Onodingene Somtochukwu <mimi.somto1@gmail.com>
2023-04-12 16:04:23 +0100

    (My first commit)

---
+++ Week_1/week_1 practice 2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#python program to print strings and tpye
+str1 = "Hi my name is Matthew. I am String"
+str2 = "Hi my name is Precious. I am also a String"
+
+#displaying string str1 and its type
+print(str1)
+print(type(str1))
+
+#displaying string str2 and its type
+print(str2)
+print(type(str2))
+
+
+# In[ ]:
+
+
+
+
