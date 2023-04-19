4aa98c694757da26da4a519dec7a46ddd0b6e81a
Onodingene Somtochukwu <mimi.somto1@gmail.com>
2023-04-19 17:41:55 +0100

    (My Third commit)

---
+++ week_4/Week_4 projects.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# PROJECT 1
+print("WELCOME TO SOMTO'S SCHOOL!")
+
+girls = ['Samantha','Jada','Jane','Claire','Elizabeth','Mary','Susan','Waje','Taibat','Lilian']
+girls_age = [17,16,17,18,16,18,17,20,19,17]
+girls_height = [5.5,6.0,5.4,5.9,5.6,5.5,6.1,6.0,5.7,5.5]
+girls_scores = [80,85,70,60,76,66,87,95,50,49]
+
+print("Name    |Age  | Height | score")
+for i in range(len(girls)):
+    print(girls[i] ,"|",girls_age[i],"|",girls_height[i],"|",girls_scores[i])
+
+boys= ['Charles','Jude','James','Kelvin','Blodun','Wale','Kunle','Matthew','Tom','Kayode']
+boys_age = [19,16,18,17,20,19,16,18,17,19]
+boys_height = [5.7,5.9,5.8,6.1,5.9,5.5,6.1,5.4,5.8,5.7]
+boys_score = [74,87,75,68,66,78,87,98,54,60]
+
+for i in range(len(boys)):
+    print(boys[i],boys_age[i],boys_height[i],boys_score[i])
+
+
+# In[ ]:
+
+
+# PROJECT 2 
+print("WELCOME TO SOMTO'S SCHOOL!")
+
+girls = ['Samantha','Jada','Jane','Claire','Elizabeth','Mary','Susan','Waje','Taibat','Lilian']
+girls_age = [17,16,17,18,16,18,17,20,19,17]
+girls_height = [5.5,6.0,5.4,5.9,5.6,5.5,6.1,6.0,5.7,5.5]
+girls_scores = [80,85,70,60,76,66,87,95,50,49]
+
+print("Name    |Age  | Height | score")
+for i in range(len(girls)):
+    print(girls[i] ,"|",girls_age[i],"|",girls_height[i],"|",girls_scores[i])
+
+boys= ['Charles','Jude','James','Kelvin','Blodun','Wale','Kunle','Matthew','Tom','Kayode']
+boys_age = [19,16,18,17,20,19,16,18,17,19]
+boys_height = [5.7,5.9,5.8,6.1,5.9,5.5,6.1,5.4,5.8,5.7]
+boys_score = [74,87,75,68,66,78,87,98,54,60]
+
+for i in range(len(boys)):
+    print(boys[i],boys_age[i],boys_height[i],boys_score[i])
+
